00cc28576ad4ae37f6cfdbf50d1bf10008334242
pounitednation <pounitednation@gmail.com>
2026-06-21 14:36:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,29 @@
 import os
+import feedparser
 import requests
+
+RSS_URL = "https://chaszmin.com.ua/category/granty-tut/feed/"
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+feed = feedparser.parse(RSS_URL)
+
+if len(feed.entries) == 0:
+    print("No entries found")
+    exit()
+
+entry = feed.entries[0]
+
+title = entry.title
+link = entry.link
+
+message = f"""📢 Новий грант
+
+{title}
+
+🔗 {link}
+"""
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
@@ -10,7 +31,7 @@ response = requests.post(
     url,
     data={
         "chat_id": CHAT_ID,
-        "text": "Тест із GitHub Actions 🚀"
+        "text": message
     }
 )
 
