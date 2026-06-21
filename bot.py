33eff0c36b3a5d232f88831fd3c02ae840645745
pounitednation <pounitednation@gmail.com>
2026-06-21 14:34:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,16 @@ import os
 import requests
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/getMe"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-response = requests.get(url)
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "Тест із GitHub Actions 🚀"
+    }
+)
 
-print("STATUS =", response.status_code)
 print(response.text)
