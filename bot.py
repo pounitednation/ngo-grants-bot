bb2fb1b106349f1eb7d1ac076f1dc15d19b2f51f
pounitednation <pounitednation@gmail.com>
2026-06-21 13:48:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import os
+import requests
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-print("TOKEN LENGTH:", len(BOT_TOKEN))
-print("CHAT ID:", CHAT_ID)
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/getMe"
 
-print(url[:50] + "...")
+r = requests.get(url)
+
+print(r.text)
