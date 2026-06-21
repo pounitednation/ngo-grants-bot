97d6dd0fd5845f04e28a565c27e89846b01307c9
pounitednation <pounitednation@gmail.com>
2026-06-21 13:42:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,4 @@
 import os
-import feedparser
-import requests
 
-RSS_URL = "https://chaszmin.com.ua/category/granty-tut/feed/"
-
-BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-feed = feedparser.parse(RSS_URL)
-
-if len(feed.entries) == 0:
-    print("No entries found")
-    exit()
-
-entry = feed.entries[0]
-
-title = entry.title
-link = entry.link
-
-message = f"📢 Новий грант\n\n{title}\n\n{link}"
-
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-response = requests.post(
-    url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-)
-
-print(response.text)
+print("TOKEN EXISTS:", bool(os.getenv("TELEGRAM_TOKEN")))
+print("CHAT EXISTS:", bool(os.getenv("TELEGRAM_CHAT_ID")))
